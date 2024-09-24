f0af9e813ef5fbe5d099f0a2890e5dace69655e0
SUHU8password613 <suhanimishra613@gmail.com>
2024-09-24 05:00:06 -0700

    conditionAndloops.js

---
+++ SECTION 1/condition.js
@@ -0,0 +1,36 @@
+n = 86;
+
+if(n%2 ===0){
+
+    message='good';
+
+    var message2 ='nice'
+    let message3 ='cool'
+
+
+
+    console.log('even');
+}else{
+    console.log('odd');
+}
+
+console.log(message);
+console.log(message2);
+
+
+
+//WAP to check if a number is divisible by both 7 &11
+ 
+const m =344;
+
+if(m % 7 === 0 && m % 11 === 0){
+console.log('Divisible by both 7 and 11');
+
+
+}else{
+    console.log(' Not Divisible by both 7 and 11');
+    
+    
+}
+
+
---
+++ SECTION 1/index.js
@@ -0,0 +1,37 @@
+console.log('Hello JavaScript!');
+    
+a=343;
+
+console.log(a);
+console.log(typeof a );
+
+message ="Be cool";
+console.log(message);
+
+
+isHungry = true;
+console.log(isHungry);
+
+[x,y,z] = [3,4,5];
+console.log(x+y+z);
+
+
+//exponent operator
+console.log(2**4);
+
+
+console.log(1+1);
+console.log(1+'1');
+console.log(1+'1'+1);
+console.log(1+1+'1');
+console.log(null+'1');
+console.log( 1==1 );
+console.log( 1=='1' );
+console.log( 1==='1');
+
+
+
+
+
+
+
